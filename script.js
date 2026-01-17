cf327731b51c1334238c147e1377c3086a04e8f6
im-abdulhanan <abdulhanan.microsoft@gmail.com>
2026-01-17 16:07:41 +0500

    files

--- script.js
+++ script.js
@@ -1,33 +1,27 @@
+// dark-light-mode-toggle
+let body = document.body
+let day_light =  document.getElementById("night")
+
+function darklight(){
+  let check = body.classList.contains("light")
+  let day_light_change = day_light.classList.contains("night") 
+  if(check){
+    body.classList.add("dark")
+    body.classList.remove("light")
+    day_light_change.classList.add("light")
+    day_light_change.classList.remove("night")
+
+  }else{
+    body.classList.add("light")
+    body.classList.remove("dark")
+    day_light_change.classList.add("night")
+    day_light_change.classList.remove("light")
+  }
+}
+
 // hero section
 // arrow img change
-
-// let img = document.getElementById("hero_img_1")
-// let arrow_right = document.getElementById("arrow_right");
-// function changeImgtoRight(){
-//     for(let i = 0; i < img.length; i++){
-//         img.src = "banner pics/banner_1.jpg" 
-//     }
-// }
-// function changeImgtoLeft(){
-//     img.src = "banner pics/banner_2.jpg" 
-// }
 let img = document.getElementById("hero_img_1");
-
-let images = [
-  "banner pics/banner_1.jpg",
-  "banner pics/banner_2.jpg",
-  "banner pics/banner_3.jpg"
-];
-
-let index = 0;
-
-setInterval(() => {
-  index++;
-
-  if(index >= images.length){
-    index = 0; // go back to first image (infinite loop)
-  }
-
-  img.src = images[index];
-}, 2000); // change every 2 seconds
-
+function changeImg(){
+  img.setAttribute("src","banner pics/banner_1.jpg")
+}
